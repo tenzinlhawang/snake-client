a1a7a259b28ec941cfdb8764e32ee80d87ff3dba
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-05 00:00:33 +0000

    Updated the client side function for snake game

--- client.js
+++ client.js
@@ -1,25 +1,39 @@
+const { time } = require('console');
+let timer = 0;
 const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
+    // host: 'localhost',
+    // port: 50541
+    host: '135.23.222.131',
+    port: 50542
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
   conn.on('data', data => console.log(data))
 
-  conn.on('connect', connectionSecured => console.log('Connection established'));
-  
-  conn.on('connect', () => conn.write('Name: TL'));
-  
+  conn.on('connect', () => {
+    
+    console.log('Connection established');
+    conn.write('Name: TLP');
 
+    setTimeout(() => {
+      conn.write('Move: up');
+    }, timer);
+    timer += 500;
+
+    setInterval(() => {
+      conn.write('Move: up');
+    }, timer);   
+
+
+  })
   return conn;
-
 }
 
   // console.log('Connecting ...');
   //  connect();
 
-   module.exports = {connect};
+   module.exports = {connect}
